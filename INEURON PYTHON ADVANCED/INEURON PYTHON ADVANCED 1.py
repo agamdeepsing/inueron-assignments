a268c8ad77f339b4f7519bf34eb357e48acc416c
agamdeepsing <93773431+agamdeepsing@users.noreply.github.com>
2022-08-20 14:10:07 -0700

    Add files via upload

---
+++ INEURON PYTHON ADVANCED/INEURON PYTHON ADVANCED 1.py
@@ -0,0 +1,21 @@
+1.main purpose of oop is to grouping of data and the functions and diving the program into small segments and making the code easier.
+
+2.it first searches for the attribute in its instance and then looks in the class it is generated from, to all super classes listed in its class header
+
+3.class is a template for making objects and objects is instance of class.
+class is created once and many objects can be created using class.
+
+4. self is the first argument for instance method and cls is the first argument for class methods which refers to class itself
+
+5. __init__ is a reseved method in python classes. It serves the role of a constructor in object oriented programming.
+
+6. to creating class instance we need to call class  by its name that we have entered while creating it and pass the arguments to the class.
+
+7.class keyword is used to make class in python.
+
+8. superclass is given as a argument to the child class. for ex i have created class person and its superclass will be class emp(person).
+
+
+
+
+
